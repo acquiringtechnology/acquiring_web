92a28e2324f91bcf433e3f126d0ca3adc11c2ad9
anvesh balaji <anveshbabu@cholamsispl.com>
2023-10-13 21:30:18 +0530

    vebiner add

--- src/components/pages/home/upCommingwebinarCards/index.js
+++ src/components/pages/home/upCommingwebinarCards/index.js
@@ -5,33 +5,34 @@ import { useRouter } from "next/router";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import { connect } from "react-redux";
-import moment from 'moment';
+import moment from "moment";
 import { bindActionCreators } from "redux";
 import * as webinarAction from "@/redux/action/webinear";
-import {setStorage} from "@/services/helperFunctions";
-import {EXIST_LOCAL_STORAGE} from "@/services/constants";
+import { setStorage } from "@/services/helperFunctions";
+import { EXIST_LOCAL_STORAGE } from "@/services/constants";
 import { useEffect } from "react";
 
-const UpcommingWebinarsCards = ({ isViewAll = false, getAllWebinear ,webinarList=[] }) => {
+const UpcommingWebinarsCards = ({
+  isViewAll = false,
+  getAllWebinear,
+  webinarList = [],
+}) => {
   const router = useRouter();
 
   useEffect(() => {
     getAllWebinear();
   }, []);
 
-const handleGetDiffMid=(fromD,toDat)=>{
-  let date1 = moment( toDat);
-  let date2 = moment(fromD);
-  return date1.diff(date2, 'm') 
-};
+  const handleGetDiffMid = (fromD, toDat) => {
+    let date1 = moment(toDat);
+    let date2 = moment(fromD);
+    return date1.diff(date2, "m");
+  };
 
-
-const handleRouteDetailPage=(data)=>{
-
-  setStorage(EXIST_LOCAL_STORAGE.WEBINAR_DETAIL ,JSON.stringify(data))
-  router.push(`/webinars/details/${data.id}`)
-}
-
+  const handleRouteDetailPage = (data) => {
+    setStorage(EXIST_LOCAL_STORAGE.WEBINAR_DETAIL, JSON.stringify(data));
+    router.push(`/webinars/details/${data.id}`);
+  };
 
   return (
     <section>
@@ -45,22 +46,9 @@ const handleRouteDetailPage=(data)=>{
             </div>
           </div>
 
-          {/* <div className="row">
-            <div className="col-md-3"> */}
-          <Swiper
-            spaceBetween={30}
-            slidesPerView={4}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-            // loop={true}
-            autoplay={{
-              delay: 1000,
-              disableOnInteraction: false,
-            }}
-            //modules={[Autoplay]}
-          >
+          <div className="row justify-content-center">
             {webinarList?.map((data, i) => (
-              <SwiperSlide key={i}>
+              <div className="col-md-3" key={i}>
                 <div className="card border-0 shadow mb-5">
                   <img
                     src={data.cardImg}
@@ -68,17 +56,21 @@ const handleRouteDetailPage=(data)=>{
                     alt={data.title}
                   />
                   <div className="card-body">
-                    <h5 className="card-title">
-                      {data.title}
-                    </h5>
+                    <h5 className="card-title">{data.title}</h5>
 
                     <div className="text-muted mb-2 w-100">
                       <span>
                         {" "}
                         <i className="fa-regular fa-clock"></i>
-                        <p className="d-inline mx-2">{handleGetDiffMid(data.sDate ,data.eDate)} Mins</p>
+                        <p className="d-inline mx-2">
+                          {handleGetDiffMid(data.sDate, data.eDate)} Mins
+                        </p>
                       </span>
-                      <a href="#" className="float-end" onClick={()=>handleRouteDetailPage(data)}>
+                      <a
+                        href="#"
+                        className="float-end"
+                        onClick={() => handleRouteDetailPage(data)}
+                      >
                         Register Now
                       </a>
                     </div>
@@ -86,11 +78,10 @@ const handleRouteDetailPage=(data)=>{
                     {/* </p> */}
                   </div>
                 </div>
-              </SwiperSlide>
+              </div>
             ))}
-          </Swiper>
-
-          {!isViewAll && (
+          </div>
+          {!isViewAll && webinarList.length > 4 && (
             <div className="row mb-4">
               <div className="col-md-12 text-center mb-4">
                 <button
