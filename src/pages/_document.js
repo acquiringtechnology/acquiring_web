97d5d81534458bae4e2bb60516b76992b92d5de0
anvesh balaji <kb.anvesh1996@gmail.com>
2025-03-10 16:39:34 +0530

    fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,7 +26,7 @@ export default function Document() {
             src="https://www.googletagmanager.com/ns.html?id=GTM-NGCMBFP7"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{display: 'none',visibility: 'hidden'}}
           ></iframe>
         </noscript>
         <Main />
