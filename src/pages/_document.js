d940defc9741ab11a0b4f89aa4a81167bd3e1203
anvesh balaji <kb.anvesh1996@gmail.com>
2025-03-10 16:46:27 +0530

    ver fasiin fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,37 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="-EewiVukAAPVxvxUF8ZcJd6vwj8JVgA11XX-HDYI1VE"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NGCMBFP7');
-              `,
-          }}
-        />
-      </Head>
+      <Head >
+      <meta name="google-site-verification" content="7LkEeErCiBKHC-1ZYXvd3lIDYiPGe7q0PvvRxL6BOoc" />
+        
+        </Head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NGCMBFP7"
-            height="0"
-            width="0"
-            style={{display: 'none',visibility: 'hidden'}}
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
